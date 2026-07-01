8bb51464b91ce86ea432275b2418854e6eb10820
hemantpant024-afk <hemantpant024@gmail.com>
2026-07-01 15:17:41 +0530

    update  python file

--- function.py
+++ function.py
@@ -1,28 +1,22 @@
 class Calculator:
-    """A simple class to demonstrate OOP and defining methods."""
-    
-    def __init__(self, brand_name):
-        # This is the constructor. It initializes instance attributes.
-        self.brand = brand_name
+    def __init__(self, value=0):
+        self.value = value
 
-    def greet_user(self):
-        """A simple method with no parameters (besides self)."""
-        print(f"Welcome to your {self.brand} calculator!")
+    def add(self, x):
+        self.value += x
+        return self.value
 
-    def add_numbers(self, num1, num2):
-        """A method that takes parameters and returns a value."""
-        result = num1 + num2
-        return result
+    def subtract(self, x):
+        self.value -= x
+        return self.value
+
+    def reset(self):
+        self.value = 0
+        return self.value
 
 
-# --- How to use the class and its functions (methods) ---
-
-# 1. Create an object (instance) of the class
-my_calc = Calculator(brand_name="Casio")
-
-# 2. Call a method that performs an action
-my_calc.greet_user()
-
-# 3. Call a method that takes arguments and returns a value
-sum_result = my_calc.add_numbers(15, 25)
-print(f"The sum is: {sum_result}")
+# Usage
+calc = Calculator()
+print(calc.add(10))       # 10
+print(calc.subtract(3))   # 7
+print(calc.reset())       # 0
