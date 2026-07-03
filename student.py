e8b581079bd5768b03e90568475f783f432530b2
hemantpant024-afk <hemantpant024@gmail.com>
2026-07-03 17:18:16 +0530

    update student.py

--- student.py
+++ student.py
@@ -3,29 +3,39 @@
 class Student:
     name: str
     age: int
-    course: str
+    branch: str
     blood_group: str
     roll_num: int
-    is_hostilier: bool
+    mobile_num: int
+
+    def __init__(self, name: str, age: int, branch: str, blood_group: str, roll_num: int, mobile_num: int):
+        self.name: str = name
+        self.age: int = age
+        self.branch: str = branch
+        self.blood_group: str = blood_group
+        self.roll_num: int = roll_num
+        self.mobile_num: int = mobile_num
 
     def display(self):
-        print("Student Details")
-        print("Name   :", self.name)
-        print("Age    :", self.age)
-        print("Course :", self.course)
+        print("Roll No:", self.roll_num)
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Branch:", self.branch)
+        print("Blood Group:", self.blood_group)
+        print("Mobile No:", self.mobile_num)
+        print("-" * 25)
 
 
-# Create an object
-student1 = Student()
+# ---- Create 5 students ----
+s1 = Student("Hemant Pant", 19, "CSE", "O+", 1, 9876543264)
+s2 = Student("Pankaj Joshi", 20, "CSE", "B+", 2, 9123456780)
+s3 = Student("Rohit Chandola", 19, "CSE", "A+", 3, 9988776655)
+s4 = Student("Sneha Negi", 20, "Civil", "AB+", 4, 9871234567)
+s5 = Student("lucky Negi", 21, "CSE", "O-", 5, 9765432109)
 
-# Assign values
-student1.name = "Hemant"
-student1.age = 19
-student1.course = "CSE"
-
-# Display details
-student1.display()
-
-
-def hello_student():
-    pass
+# ---- Display all students ----
+s1.display()
+s2.display()
+s3.display()
+s4.display()
+s5.display()
